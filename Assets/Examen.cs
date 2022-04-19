fffa5a4f3ce0700bc112d8e602a3b2cd214e1cb1
FelipeMaciel05 <felimorita05@gmail.com>
2022-04-19 13:59:57 -0300

    PRUEBA FINALIZADA

--- Assets/Examen.cs
+++ Assets/Examen.cs
@@ -2,17 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Examen : MonoBehaviour
 {
+    public string ingresasabor = "";
+    public float canthelado;
     // Start is called before the first frame update
     void Start()
     {
         
-    }
+        float descuento;
+        float resultado;
+        if (canthelado < 250 || canthelado > 3000)
+        {
+            Debug.Log("No se pueden esos valores");
+        }
+        else
+        {
+            switch (ingresasabor)
+            {
+                case "CHO":
+                resultado = (canthelado / 1000) * 500;
+                Debug.Log("Sale $" + resultado);
+                break;
+                case "FRU":
+                resultado = (canthelado / 1000) * 500;
+                descuento = resultado * 0.9f;
+                Debug.Log("Sale $" + descuento);
+                break;
+                case "DLL":
+                resultado = (canthelado / 1000) * 500;
+                Debug.Log("Sale $" + resultado);
+               break;
+                default:
+                    Debug.Log("ingreso otro gusto no disponible");
+                    break;
+            }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
